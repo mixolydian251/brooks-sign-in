3244ce186ba3652f7a7baf893f74d96c131f3795
mixolydian251 <tjhmusic11@gmail.com>
2017-12-10 00:33:41 -0500

    prettier

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+const privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
 const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 const path = require('path');
 const compression = require('compression');
 
-const credentials = {key: privateKey, cert: certificate};
+const credentials = { key: privateKey, cert: certificate };
 const express = require('express');
 const app = express();
 app.use(compression());
@@ -15,7 +15,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'))
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 const httpServer = http.createServer(app);
